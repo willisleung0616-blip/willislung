44fc454cb9222ff331f37bb96ade88dc6ccbe5ae
willisleung0616-blip <willisleung0616@gmail.com>
2025-08-30 17:40:22 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { App } from "./screens/PhoneHomePage";
 
 createRoot(document.getElementById("app")).render(
   <StrictMode>
